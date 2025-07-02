4f83777e1cc3f1c76e21136c22f39e28a095538d
Partac Alexis <alexismateipartac@gmail.com>
2025-07-02 19:27:37 +0300

    actualizari

--- src/app/api/payment-card/route.ts
+++ src/app/api/payment-card/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY || '', {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
     apiVersion: '2025-05-28.basil',
 });
 
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
-            success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/success`,  //?session_id={CHECKOUT_SESSION_ID}
+            success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/success`,
             cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout/cancel`,
         });
 
