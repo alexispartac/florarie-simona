62aa13686c73d54e9df0b0bd1ba6793f478f7b06
Partac Alexis Matei <alexismateipartac@gmail.com>
2025-12-30 14:54:02 +0200

    change middleware with proxi

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
   
   // If it's an API route for admin, check the auth header
